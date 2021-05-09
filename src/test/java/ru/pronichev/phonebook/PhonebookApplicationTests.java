e06d6344c2437fb87a732f173f56bcfd0bba3f09
Pronichev Aleksej <pronichevap@gmail.com>
2021-05-09 13:08:12 +0300

    Initial commit

---
+++ src/test/java/ru/pronichev/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pronichev.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
