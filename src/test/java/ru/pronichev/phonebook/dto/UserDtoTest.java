608cf7e53cf7eb106eb64ff501a6dd293cc8302c
Pronichev Aleksej <pronichevap@gmail.com>
2021-05-10 16:18:40 +0300

    add serialisation test for user

--- src/test/java/ru/pronichev/phonebook/dto/UserDtoTest.java
+++ src/test/java/ru/pronichev/phonebook/dto/UserDtoTest.java
@@ -1,11 +1,21 @@
 package ru.pronichev.phonebook.dto;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 import ru.pronichev.phonebook.entities.User;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@JsonTest
 class UserDtoTest {
+
+    @Autowired
+    private JacksonTester<UserDto> jacksonTester;
 
     @Test
     void toDto() {
@@ -26,5 +36,23 @@ class UserDtoTest {
 
         assertEquals(user.getId(), userDto.getId());
         assertEquals(user.getLogin(), userDto.getLogin());
+    }
+
+    @Test
+    void testSerializeUser() throws IOException {
+        User user = new User();
+        user.setLogin("TestLogin");
+        user.setId(87L);
+        UserDto userDto = UserDto.toDto(user);
+
+        assertThat(this.jacksonTester.write(userDto)).isStrictlyEqualToJson("user-test.json");
+    }
+
+    @Test
+    void testDeserializeUser() throws IOException {
+        UserDto userDto = this.jacksonTester.read("user-test.json").getObject();
+
+        assertEquals(87L, userDto.getId());
+        assertEquals("TestLogin", userDto.getLogin());
     }
 }
